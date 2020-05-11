9be13550f2c7098e823b871e59e534d847408b92
zhuwt <893850280@qq.com>
2020-05-11 19:19:52 +0800

    项目集成Redux Redux-devtools在浏览器中查看变化

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -3,6 +3,6 @@
  */
 import { createStore } from 'redux'
 import reducer from '../reducer'
-// import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-export default (prevState) => createStore (reducer, prevState)
+export default () => createStore (reducer, composeWithDevTools())
